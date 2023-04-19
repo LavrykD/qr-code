66431a20cc4601afa671a46b724de22157b55673
Dmytro Lavryk <62079347+LavrykD@users.noreply.github.com>
2023-04-19 10:03:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import flet as ft
 import pyqrcode
 import base64
 from io import BytesIO
+import time
 
 
 def main(page: ft.Page):
@@ -9,22 +10,23 @@ def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
     def generate_qr_code(event_control):
-        qr = pyqrcode.create(f"{user_input.value}", version=6 if len(user_input.value) < 60 else 12)
+        """Generate the QR Code by using pyqrcode lib and append it to the program flow.
+        """
+        qr = pyqrcode.create(f"{user_input.value}", version=8 if len(
+            user_input.value) < 70 else 14)
+
         # Converting qrcode into base64 string
         buf = BytesIO()
-        qr.svg(buf, scale=7)
+        qr.svg(buf, scale=6)
 
         # Append generated QR Code to flow and delete the last created
         if len(page.controls) > 2:
-            page.controls.pop(-2)
-        page.controls.append(
-            ft.Image(src_base64=base64.b64encode(buf.getvalue()).decode('utf-8'))
+            page.controls.pop(-1)
+        page.add(
+            ft.Image(src_base64=base64.b64encode(
+                buf.getvalue()).decode('utf-8'))
         )
 
-        # Append information bar
-        page.controls.append(
-            ft.SnackBar(ft.Text("QR Code is generated!"), open=True)
-        )
         page.update()
 
     user_input = ft.TextField(
